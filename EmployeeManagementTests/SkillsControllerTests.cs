a4a46646439cff723133865028d80f68adb317cf
Apo Mene <apomene@yahoo.gr>
2025-08-26 11:24:47 +0300

    Added Test Project

---
+++ EmployeeManagementTests/SkillsControllerTests.cs
@@ -0,0 +1,16 @@
+﻿namespace EmployeeManagementTests
+{
+    public class SkillsControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
