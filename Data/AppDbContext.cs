82cfd5f55f8eb832733971ebcb6a629e25871af3
Apo Mene <apomene@yahoo.gr>
2025-09-16 04:08:36 +0300

    final fixes after manual testing

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -44,7 +44,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         modelBuilder.Entity<Skill>().HasData(
             new Skill { Id = 1, Name = "C#", Description = "Programming in C#", CreatedAt = new DateTime(2025, 1, 1) },
             new Skill { Id = 2, Name = "SQL", Description = "Database querying and design", CreatedAt = new DateTime(2025, 1, 1) },
-            new Skill { Id = 3, Name = "JavaScript", Description = "Frontend and backend scripting", CreatedAt = new DateTime(2025, 1, 1) },
+            new Skill { Id = 3, Name = "JavaScript", Description = "Frontend and backend scripting", CreatedAt = new DateTime(1995, 1, 1) },
             new Skill { Id = 4, Name = "Project Management", Description = "Agile and Scrum methodologies", CreatedAt = new DateTime(2025, 1, 1) },
             new Skill { Id = 5, Name = "Cloud Computing", Description = "Azure and AWS services", CreatedAt = new DateTime(2025, 1, 1) }
         );
---
+++ Migrations/20250910074441_ChangedJStime.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedJStime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(1995, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
